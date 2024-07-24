cf593628c0e569aed6b16712477e74c6c7fc657c
Julio Cesar <julio0033@hotmail.com>
2024-07-24 20:44:05 -0300

    criação da página Ajuda
    Co-authored-by: João Pedro <joaopedroblome78@gmail.com>

---
+++ app/help/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ContentCard from '../components/ContentCard'
+import InfoIcon from '../components/icons/InfoIcon'
+
+const Help = () => {
+  return (
+    <>
+      <ContentCard 
+      title='Problemas detectados?' 
+      text='Nos envie um email no endereço: direitofluxoapp@unb.com relatando o erro.'
+      breadCrumbText='Ajuda'
+      />
+    </>
+  )
+}
+
+export default Help
