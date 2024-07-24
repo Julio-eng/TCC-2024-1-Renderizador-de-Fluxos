91c99b1183e01709d0d3dd772d014b469c48f0a7
Julio Cesar <julio0033@hotmail.com>
2024-07-24 20:30:47 -0300

    Criação da página Sobre
    Co-authored-by: João Pedro <joaopedroblome78@gmail.com>

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ContentCard from '../components/ContentCard'
+
+const About = () => {
+  return (
+    <>
+      <ContentCard 
+        title='SOBRE NÓS'
+        text='Somos estudantes de Engenharia de Software na Universidade de Brasília e de Direito na Universidade de São Paulo. Nossa equipe dedicada mapeou os fluxos trabalhistas de diversas profissões, tornando essas informações acessíveis a todos através deste aplicativo. Nosso objetivo é democratizar o acesso à informação, proporcionando uma ferramenta útil e eficaz para consulta. Obrigado por fazer parte dessa jornada!'
+        breadCrumbText='Sobre'
+      />
+    </>
+  )
+}
+
+export default About
