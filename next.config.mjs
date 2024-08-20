6f453f2fe9f26573f046cd142cd864253437de5f
Julio Cesar <julio0033@hotmail.com>
2024-08-20 15:21:21 -0300

    Ajustantando configurações do PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
