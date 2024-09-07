75dfb0d0a7b47b0b45d20b819480deb485d1329f
Julio Cesar <julio0033@hotmail.com>
2024-09-07 15:35:41 -0300

    Resolvendo CORS da API next

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://develop--flow-render.netlify.app" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+};
 
 const withPWA = withPWAInit({
   dest: "public",
