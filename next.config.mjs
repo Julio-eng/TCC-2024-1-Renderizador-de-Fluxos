a0dd98335a909d8f3a5e896bc46a4f5a9544d40d
Julio Cesar <julio0033@hotmail.com>
2024-09-10 19:19:33 -0300

    Configurações de CORS e caminhos para imagens

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,19 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'lh*.googleusercontent.com',
+    }],
+  },
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://develop--flow-render.netlify.app" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "https://develop--flow-render.netlify.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
